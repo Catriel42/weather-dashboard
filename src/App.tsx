daf28b79d824eaf4d205de78df53cd34273eeebc
Catriel42 <catopereira72@gmail.com>
2026-09-19 01:21:26 -0400

    refactor: update App component to functional component declarations

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import "./App.css";
 import { WeatherDashboard } from "./components/WeatherDashboard";
 
-function App() {
+export const App = () => {
   return <WeatherDashboard />;
-}
-
-export default App;
+};
