2a333c2c0af7660a0e301dddb008c9befb3887d0
Catriel42 <catopereira72@gmail.com>
2026-09-23 02:08:35 -0400

    feat: Adding router dom library and wrapper BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
+import { Routes, Route } from 'react-router-dom'
+
 import './App.css'
+import { Navbar } from './components/Navbar'
 import { WeatherDashboard } from './components/WeatherDashboard'
+import { EmptyState } from './components/EmptyState'
+import { NotFound } from './components/NotFound'
 
 export const App = () => {
-  return <WeatherDashboard />
+
+  return (
+    <section>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<WeatherDashboard />} />
+        <Route path="/about" element={<EmptyState />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </section>
+  )
 }
