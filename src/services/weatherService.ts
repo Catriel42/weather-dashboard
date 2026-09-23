134fec504f80aa90ce23c39d5f987e0ac16d1272
Catriel42 <catopereira72@gmail.com>
2026-09-23 16:24:48 -0400

    feat(services): add getForecast service with coordinate support

--- src/services/weatherService.ts
+++ src/services/weatherService.ts
@@ -1,8 +1,15 @@
+import type {
+  City,
+  WeatherData,
+  ForecastItem,
+  ForecastResponse,
+} from '../types/weather'
+
 const API_KEY = '54763932f62e7948dce4e505f2d36571'
 const BASE_URL_GEO = 'https://api.openweathermap.org/geo/1.0'
 const BASE_URL_WEATHER = 'https://api.openweathermap.org/data/2.5'
 
-export const searchCities = async (query: string) => {
+export const searchCities = async (query: string): Promise<City[]> => {
   try {
     const response = await fetch(
       `${BASE_URL_GEO}/direct?q=${query}&limit=5&appid=${API_KEY}`,
@@ -12,7 +19,7 @@ export const searchCities = async (query: string) => {
       throw new Error(response.statusText)
     }
 
-    const data = await response.json()
+    const data: City[] = await response.json()
     return data
   } catch (error) {
     console.error('Problem during fetch:', error)
@@ -20,7 +27,10 @@ export const searchCities = async (query: string) => {
   }
 }
 
-export const getCurrentWeather = async (lat: number, lon: number) => {
+export const getCurrentWeather = async (
+  lat: number,
+  lon: number,
+): Promise<WeatherData | null> => {
   try {
     const response = await fetch(
       `${BASE_URL_WEATHER}/weather?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`,
@@ -30,10 +40,37 @@ export const getCurrentWeather = async (lat: number, lon: number) => {
       throw new Error(response.statusText)
     }
 
-    const data = await response.json()
+    const data: WeatherData = await response.json()
     return data
   } catch (error) {
     console.error('Error during fetch:', error)
     return null
+  }
+}
+
+export const getForecast = async (
+  lat: number,
+  lon: number,
+): Promise<ForecastItem[]> => {
+  try {
+    const response = await fetch(
+      `${BASE_URL_WEATHER}/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`,
+    )
+
+    if (!response.ok) {
+      throw new Error(`Error ${response.status}: ${response.statusText}`)
+    }
+
+    const data: ForecastResponse = await response.json()
+
+    // Filter 40 reports, only have 18:00:00
+    const filteredForecast: ForecastItem[] = data.list.filter(
+      (item: ForecastItem) => item.dt_txt.includes('18:00:00'),
+    )
+
+    return filteredForecast
+  } catch (error) {
+    console.error('Error fetching forecast:', error)
+    return []
   }
 }
