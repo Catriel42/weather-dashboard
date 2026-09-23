7049bf6d2e62e2afa4e2081d76b14f4bc4daaae4
Catriel42 <catopereira72@gmail.com>
2026-09-23 16:24:16 -0400

    feat(types): add ForecastItem and ForecastResponse interfaces

--- src/types/weather.ts
+++ src/types/weather.ts
@@ -46,3 +46,63 @@ export interface WeatherData {
   timezone: number
   name: string
 }
+
+export interface ForecastItem {
+  dt: number
+  dt_txt: string
+  main: {
+    temp: number
+    feels_like: number
+    temp_min: number
+    temp_max: number
+    pressure: number
+    humidity: number
+    sea_level?: number
+    grnd_level?: number
+    temp_kf?: number
+    dew_point?: number
+  }
+  weather: Array<{
+    id: number
+    main: string
+    description: string
+    icon: string
+  }>
+  clouds: {
+    all: number
+  }
+  wind: {
+    speed: number
+    deg: number
+    gust?: number
+  }
+  visibility: number
+  pop: number
+  rain?: {
+    '3h'?: number
+  }
+  sys: {
+    pod: string
+  }
+}
+
+export interface ForecastResponse {
+  cod: string
+  message: number | string
+  cnt: number
+  list: ForecastItem[]
+  city: {
+    id: number
+    name: string
+    coord: {
+      lat: number
+      lon: number
+    }
+    country: string
+    population?: number
+    timezone: number
+    sunrise: number
+    sunset: number
+  }
+}
+
